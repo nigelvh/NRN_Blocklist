5aed624a99b3a23d0b785993c081c983f44d8bf3
Nigel Vander Houwen <nigel@nigelvh.com>
2020-09-23 13:42:15 -0700

    Added simple/dumb example script to submit entries to blacklist.

---
+++ submit_blacklist.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import sys
+import requests
+
+API_ENDPOINT = "https://www.nullroutenetworks.com/blacklist/submit.php"
+API_KEY = "YOUR_API_KEY"
+
+if len(sys.argv) < 2:
+    print("Provide the IP to blacklist as the first command line argument.")
+    exit()
+
+# The host to blacklist
+HOST = sys.argv[1]
+# The time in seconds before expiry
+EXPIRES = "86400"
+# Trigger category ABUSE/HONEYPOT/etc
+TYPE = "ABUSE"
+# Trigger service SSH/HTTP/HTTPS/ASTERISK/etc
+SERVICE = "SSH"
+
+data = {'key':API_KEY,
+        'host':HOST,
+        'expires':EXPIRES,
+        'type':TYPE,
+        'service':SERVICE}
+
+r = requests.post(url = API_ENDPOINT, data = data)
+
+print(r.text)
